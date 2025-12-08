6287b42524fb32b00846a11e0baf19a160400190
Sanja Daic <sanja.daic86@gmail.com>
2025-12-08 15:13:32 +0100

    creating header part I

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+import styles from "./App.module.scss";
 
 function App() {
-  return <div>Start</div>;
+  return (
+    <header>
+      <div className={styles.Header}>
+        <img src="images/Logo-02.png" alt="Logo" />
+        <nav>
+          <ul className={styles.Links}>
+            <li>
+              <a href="https://www.codecademy.com/learn">Home</a>
+            </li>
+            <li>
+              <a href="https://www.codecademy.com/catalog">Menu</a>
+            </li>
+            <li>
+              <a href="https://www.codecademy.com/catalog">Catering</a>
+            </li>
+            <li>
+              <a href="https://www.codecademy.com/catalog">Page</a>
+            </li>
+            <li>
+              <a href="https://www.codecademy.com/catalog">Blog</a>
+            </li>
+            <li>
+              <a href="https://www.codecademy.com/catalog">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div>Call us</div>
+      </div>
+    </header>
+  );
 }
 
 export default App;
