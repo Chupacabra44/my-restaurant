9706a891597e7e9ba031a9540485ce42a4b22acd
Sanja Daic <sanja.daic86@gmail.com>
2025-12-16 11:16:18 +0100

    fetching images from my json file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import Header from "./components/Header/Header";
 import styles from "./App.module.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [foodImages, setFoddImages] = useState([]);
+
+  useEffect(() => {
+    async function fetchImages() {
+      try {
+        const response = await fetch("foodImages.json");
+        if (!response.ok) {
+          throw new Error("No image!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        setFoddImages(data);
+        return data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchImages();
+  }, []);
+
   return (
     <>
       <Header />
@@ -15,6 +37,18 @@ function App() {
           <a href="/">Order Now</a>
           <a href="tel:+8888075000">(888) 807-5000</a>
         </div>
+        <section style={{ marginTop: "150px", height: "100vh" }}>
+          <div className={styles.imageGrid}>
+            {foodImages.map((image) => (
+              <img
+                key={image.id}
+                className={`${styles.imageItem} ${styles.slideUp}`}
+                src={image.image}
+                alt={image.alt}
+              />
+            ))}
+          </div>
+        </section>
       </main>
     </>
   );
