c6315ca47e4f14293eeb94a93f5ae981cf3bc676
Sanja Daic <sanja.daic86@gmail.com>
2025-12-22 14:59:30 +0100

    adding react-router-dom and adding About Us page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import styles from "./App.module.scss";
@@ -12,6 +13,7 @@ import FoodImages from "./components/FoodImages/FoodImages.jsx";
 import Menu from "./components/Menu/Menu.jsx";
 import Form from "./components/Form/Form.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import AboutUs from "./components/AboutUs/AboutUs.jsx";
 
 function App() {
   const [foodImages, setFoddImages] = useState([]);
@@ -61,120 +63,128 @@ function App() {
   };
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main>
-        <h1 className={styles.h1}>
-          <span>SIZZLING BURGERS,</span>
-          <br />
-          Multiple Locations Near You!
-        </h1>
-        <div className={styles.OrderLinks}>
-          <a href="/">Order Now</a>
-          <a href="tel:+8888075000">(888) 807-5000</a>
-        </div>
-        <section style={{ marginTop: "150px", height: "100vh" }}>
-          <FoodImages foodImages={foodImages} />
-        </section>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1 className={styles.h1}>
+                <span>SIZZLING BURGERS,</span>
+                <br />
+                Multiple Locations Near You!
+              </h1>
+              <div className={styles.OrderLinks}>
+                <a href="/">Order Now</a>
+                <a href="tel:+8888075000">(888) 807-5000</a>
+              </div>
+              <section style={{ marginTop: "150px", height: "100vh" }}>
+                <FoodImages foodImages={foodImages} />
+              </section>
 
-        <Menu
-          isActive={isActive}
-          setIsActive={setIsActive}
-          burgers={burgers}
-          breakfast={breakfast}
+              <Menu
+                isActive={isActive}
+                setIsActive={setIsActive}
+                burgers={burgers}
+                breakfast={breakfast}
+              />
+              <section className={styles.Catering}>
+                <h2>
+                  Enjoy our delicious <br /> burgers together.
+                </h2>
+
+                <div className={styles.CateringImgContainer}>
+                  <div>
+                    <img
+                      className={styles.Test}
+                      src="/images/Foodrest-jpg-08.jpg"
+                      alt="Foodrest8"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      className={styles.Test2}
+                      src="/images/Foodrest-jpg-09.jpg"
+                      alt="Foodrest9"
+                    />
+                  </div>
+                </div>
+
+                <div className={styles.CateringCircle}>
+                  <div>
+                    <h2>Catering</h2>
+                    <p>
+                      We offer reasonable pricing, great service, <br /> and a
+                      wide variety of cuisines – <br /> appetizers, meats
+                      seafoods, and more.
+                    </p>
+                  </div>
+
+                  {catering.map((cat) => (
+                    <Catering key={cat.id} cat={cat} />
+                  ))}
+                </div>
+              </section>
+              <section className={styles.CarouselContainer}>
+                <CarouselReview />
+                <hr />
+                <div className={styles.CounterContainer}>
+                  <AnimatedCounter
+                    end={20}
+                    textBold={"Years in Business "}
+                    textNormal={"Years of Culinary Excellence"}
+                  />
+                  <AnimatedCounter
+                    end={105}
+                    textBold={"Events Catered "}
+                    textNormal={"Successful Events Catered"}
+                  />
+                  <AnimatedCounter
+                    end={91}
+                    label={"K"}
+                    textBold={"Catering Orders Delivered  "}
+                    textNormal={"Catering Orders Completed"}
+                    breakWord
+                  />
+                  <AnimatedCounter
+                    end={5}
+                    textBold={"Branches "}
+                    textNormal={"Locations and Growing"}
+                  />
+                </div>
+              </section>
+              <section className={styles.Accordion}>
+                <div className={styles.Dashed}>
+                  <span>FAQ Questions</span>
+                </div>
+                <h3 className={styles.AccordionTitle}>We’re Here to Help</h3>
+
+                <AccordionTabs setActiveTab={setActiveTab} />
+
+                {faq[activeTab] && (
+                  <div className={styles.FadeIn} key={activeTab}>
+                    {faq[activeTab].map((item, index) => (
+                      <Accordion
+                        key={index}
+                        accordionHeaderText={item.question}
+                        accordionContentText={item.answer}
+                        isOpen={openIndex === index}
+                        onToggle={() => handleToggle(index)}
+                      />
+                    ))}
+                  </div>
+                )}
+              </section>
+              <Partners />
+              <Form />
+            </main>
+          }
         />
-        <section className={styles.Catering}>
-          <h2>
-            Enjoy our delicious <br /> burgers together.
-          </h2>
-
-          <div className={styles.CateringImgContainer}>
-            <div>
-              <img
-                className={styles.Test}
-                src="/images/Foodrest-jpg-08.jpg"
-                alt="Foodrest8"
-              />
-            </div>
-            <div>
-              <img
-                className={styles.Test2}
-                src="/images/Foodrest-jpg-09.jpg"
-                alt="Foodrest9"
-              />
-            </div>
-          </div>
-
-          <div className={styles.CateringCircle}>
-            <div>
-              <h2>Catering</h2>
-              <p>
-                We offer reasonable pricing, great service, <br /> and a wide
-                variety of cuisines – <br /> appetizers, meats seafoods, and
-                more.
-              </p>
-            </div>
-
-            {catering.map((cat) => (
-              <Catering key={cat.id} cat={cat} />
-            ))}
-          </div>
-        </section>
-        <section className={styles.CarouselContainer}>
-          <CarouselReview />
-          <hr />
-          <div className={styles.CounterContainer}>
-            <AnimatedCounter
-              end={20}
-              textBold={"Years in Business "}
-              textNormal={"Years of Culinary Excellence"}
-            />
-            <AnimatedCounter
-              end={105}
-              textBold={"Events Catered "}
-              textNormal={"Successful Events Catered"}
-            />
-            <AnimatedCounter
-              end={91}
-              label={"K"}
-              textBold={"Catering Orders Delivered  "}
-              textNormal={"Catering Orders Completed"}
-              breakWord
-            />
-            <AnimatedCounter
-              end={5}
-              textBold={"Branches "}
-              textNormal={"Locations and Growing"}
-            />
-          </div>
-        </section>
-        <section className={styles.Accordion}>
-          <div className={styles.Dashed}>
-            <span>FAQ Questions</span>
-          </div>
-          <h3 className={styles.AccordionTitle}>We’re Here to Help</h3>
-
-          <AccordionTabs setActiveTab={setActiveTab} />
-
-          {faq[activeTab] && (
-            <div className={styles.FadeIn} key={activeTab}>
-              {faq[activeTab].map((item, index) => (
-                <Accordion
-                  key={index}
-                  accordionHeaderText={item.question}
-                  accordionContentText={item.answer}
-                  isOpen={openIndex === index}
-                  onToggle={() => handleToggle(index)}
-                />
-              ))}
-            </div>
-          )}
-        </section>
-        <Partners />
-        <Form />
-      </main>
+        <Route path="/aboutus" element={<AboutUs />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
